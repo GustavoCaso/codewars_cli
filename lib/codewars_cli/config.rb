242971bd1c014f6275c2beeeff4e14a3773c4703
GustavoCaso <gustavocaso@gmail.com>
2016-01-01 17:47:17 +0100

    Added new configuration command for saving generated files

--- lib/codewars_cli/config.rb
+++ lib/codewars_cli/config.rb
@@ -24,5 +24,18 @@ module CodewarsCli
     def language(language)
       Configuration.write(language, :language, options)
     end
+
+    desc 'folder FOLDER', 'Set the global configuration for folder where all the kata information will be stored'
+    long_desc <<-LONGDESC
+      `folder FOLDER` will store the prefer location for file information.
+
+      It will create a folder for each language that you train,
+      containing the markdown file with all the information about the kata,
+      and the file to complete the kata
+    LONGDESC
+    method_options update: :boolean
+    def folder(folder)
+      Configuration.write(folder, :folder, options)
+    end
   end
 end
