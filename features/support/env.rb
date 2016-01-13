ac0d432c2f2807aa48a5341a1855069a275b3be0
GustavoCaso <gustavocaso@gmail.com>
2016-01-13 23:14:58 +0100

    Remove extra configuration for mocking ENV[HOME] aruba handle it

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,18 +2,6 @@ require 'codewars_cli/runner'
 require 'aruba/cucumber'
 require 'aruba/in_process'
 
-Before do
-  @real_path = ENV['HOME']
-  @fake_home = File.join('/tmp','fake_home')
-  FileUtils.mkdir_p(@fake_home)
-  set_environment_variable 'HOME', @fake_home
-end
-
-After do
-  ENV['HOME'] = @real_home
-  FileUtils.rm_rf @fake_home, :secure => true
-end
-
 Aruba.configure do |config|
   config.command_launcher = :in_process
   config.main_class = CodewarsCli::Runner
